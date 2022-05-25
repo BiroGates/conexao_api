72b6d69b7249112701c267a64d08a8f38e47be8f
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-05-25 19:53:23 -0300

    cor primaria

--- src/pages/CorPrimaria/index.js
+++ src/pages/CorPrimaria/index.js
@@ -3,24 +3,35 @@ import axios from "axios"
 import { useState } from "react";
 
 export default function CorPrimaria() {
-    const[cor, setCor] = useState('');
-    const[resposta, setResposta] = useState('');
+    const [cor, setCor] = useState('');
+    const [resposta, setResposta] = useState('');
 
     async function verificacaoCorPrimaria() {
-        const resp = await axios.get('http://localhost:5000/dia2/corprimaria' + cor);
+        const resp = await axios.get('http://localhost:5000/dia2/corprimaria/:' + cor);
 
-        if(resp.data.primaria === true) {
+        if (resp.data.x === true) {
             setResposta('Sim, é uma cor Primaria');
         }
-        else{
-            setResposta('Não, não é uma cor nesse carai');
+        else {
+            setResposta('Não, não é uma cor Primaria');
         }
 
     }
     return (
-        <div>
-            Aloha
-        </div>
+        <main>
+            <div>
+                <h1>Cor Primaria</h1>
+            </div>
+            <div>
+                cor: <input type="text" value={cor} onChange ={ e => setCor(e.target.value)} />
+            </div>
+            <div>
+                <button onClick={verificacaoCorPrimaria}> Verificar </button>
+            </div>
+            <div>
+                é cor Primaria? {resposta}
+            </div>
+        </main>
     )
 
 }
