deab379b2959c6bdb7dc0059baa2861629eda2ab
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-05-25 21:11:41 -0300

    Alteração na home e ajuste nas rotas

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,8 +6,14 @@ export default function index() {
             <h1>Pagina Home</h1>
             <h2>Ir para:</h2>
             <ul>
-                <li> <Link to = '/CorPrimaria'>teste da CorPrimaria </Link> </li>
+                <li> <Link to = '/corPrimaria'>teste da CorPrimaria </Link> </li>
+                <li> <Link to = '/dobro'>Calculo do dobro de um numero</Link></li>
+                <li> <Link to = '/somarQuery'>Soma de dois numeros</Link></li>
+                <li> <Link to = '/somarBody'>Soma de dois numeros</Link></li>
+                <li> <Link to = '/media'>Calculo da media de 3 notas</Link></li>
                 <li> <Link to = '/IngressoCinema'>Calculo do Ingresso Cinema</Link></li>
+                <li> <Link to = '/Frequencia'>Frequencia em que um caracter aparece</Link></li>
+                <li> <Link to = '/medirTemperatura'>Calculo de estado febril</Link></li>
             </ul>
         </main>
     )
