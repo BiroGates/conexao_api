b01e44c04cf190223cf314a5838dce03d00c9ddc
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-05-25 21:17:27 -0300

    Tabuada done

---
+++ src/pages/Tabuada/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+export default function Tabuada() {
+    const [value, setValue] = useState(0);
+    const [result, setResult] = useState([]);
+
+    async function handleTabuada (){
+        let resp = await axios.get(`http://localhost:5000/frequencia/q?numero`);            
+        setResult(resp.data.x);
+    }
+
+    return (
+        <div>
+            <div> Programa da tabuada: </div>
+            <input style={{display: "block"}} type="number" value={value} onChange={(e) => setValue(e.target.value)} />
+            <div> {result.map((item)=>{
+                return(
+                    <div> { item } </div>
+                )
+            })} 
+            </div>
+        </div>
+    )
+}
