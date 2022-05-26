a01ca23c049cb833f4a604b5cccd7ce4d5f50079
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-05-25 21:03:45 -0300

    medir temperatura

---
+++ src/pages/MedirTemperatura/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import React from 'react'
+
+import { useState } from 'react'
+
+export default function MedirTemperatura() {
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState();
+
+    async function handleFebre(){
+        let resp = await axios.get(`http://localhost:5000/medirtemperatura/q?t=${value}`); 
+        resp.data.x ? setResult('Você esta com febre!') : setResult('Você não esta com febre!');
+    }
+
+    return (
+    <div>
+        <div> PROGRAMA DETERMINAR ESTADO FEBRIL: </div>
+        <input style={{display: "block"}} type="text" value={value} />
+        <div style={{display: "block"}} > { result } </div>
+    </div>
+  )
+}
